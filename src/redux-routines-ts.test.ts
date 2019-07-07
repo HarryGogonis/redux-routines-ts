12e5122e0f5f9df4de0bab1f8949ca3268cd0827
Harry Gogonis <hgogonis@gmail.com>
2019-07-07 13:10:57 -0500

    test(cov): add coverage for module file

---
+++ src/redux-routines-ts.test.ts
@@ -0,0 +1,7 @@
+import { createRoutine } from './redux-routines-ts'
+
+describe('redux-routines-ts', () => {
+  it('exports createRoutine', () => {
+    expect(createRoutine).toBeDefined()
+  })
+})
